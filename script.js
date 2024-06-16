a07b12c7cbad36d2aa4af311c2244765c77efa4c
hellafi1992 <121656343+hellafi1992@users.noreply.github.com>
2024-06-16 04:33:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('login-form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const errorMessage = document.getElementById('error-message');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const username = usernameInput.value.trim();
+	const password = passwordInput.value.trim();
+
+	if (username === 'admin' && password === 'password') {
+		// Login successful, redirect to admin dashboard
+		window.location.href = 'index0.html';
+	} else {
+		errorMessage.textContent = 'خطئ في كلمة المرور !';
+	}
+});
